194be1c82bf16e6afbba53b8a80f049eb2413b08
Kaouthar82 <kaoutharahmami2@gmail.com>
2025-01-15 17:13:38 +0100

    Ajout de nouvelles routes et configuration dans MynewController et services.yaml

--- src/Controller/MynewController.php
+++ src/Controller/MynewController.php
@@ -2,24 +2,30 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class MynewController extends AbstractController
 {
-    #[Route('/mynew', name: 'app_mynew')]
-    public function index(): Response
+    #[Route('/hello', name: 'app_hello')]
+    public function hello(): Response
     {
-        return $this->render('mynew/index.html.twig', [
-            'controller_name' => 'MynewController',
-        ]);
+        return new Response('<h1>Hello World!</h1>');
+    }
+
+    #[Route('/hello/{name}', name: 'app_hello_dynamic')]
+    public function helloDynamic(string $name): Response
+    {
+        return new Response("<h1>Hello, $name!</h1>");
+    }
+
+    #[Route('/number/{number}', name: 'app_number', requirements: ['number' => '\d+'])]
+    public function showNumber(int $number): Response
+    {
+        return new Response("<h1>Le numéro est : $number</h1>");
     }
 }
 
-#[Route('/greet/{name}', name: 'greet', defaults: ['name' => 'World'])]
-public function greet(string $name): Response
-{
-    return new Response("<h1>Hello, $name!</h1>");
-}
+
 
